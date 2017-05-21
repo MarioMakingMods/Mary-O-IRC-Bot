25837ba7ba71929d20523fce0fd11ea0ea621517
maorninja <maorninja@outlook.com>
2017-05-20 21:36:36 -0700

    Update help.js
    This is supposed to be the IRC bot for MaryO of our discord server, so we don't need this...

--- modules/help.js
+++ modules/help.js
@@ -8,8 +8,7 @@ exports.module = function() {
 		var chan = this.channel;
 		fs.readFile('./package.json', 'utf-8', function(err, data) {
 			if (!err) {
-				chan.say(settings.globalNick + " v" + JSON.parse(data).version + " by Dirbaio, Nina, LifeMushroom, and AlphaTech. Running on Node.js " + process.versions.node + " (" + os.type() + " " + os.release() + " " + os.arch() + ").");
-				chan.say("For a list of available commands, check http://v.gd/TheBotCommands");
+				chan.say("For a list of available commands for MaryO, check http://v.gd/TheBotCommands");
 			} else {
 				console.err("Error opening ./package.js... Did you delete it?")
 			}
