7b82f629fa39ebbc212a7bb9df469abebd949fde
maorninja <maorninja@outlook.com>
2017-05-29 08:37:40 -0700

    Create board.js
    Make it link to the board.

---
+++ modules/board.js
@@ -0,0 +1,8 @@
+"use strict";
+// Gives the user a link to the forum
+
+exports.module = function() {
+	this.onCommand_board = function(nick, command) {
+		this.channel.say("Here is a link to the board: http://mariomakingmods.net/.");
+	};
+};
